ad927eb3654ce94f19674b0ff886770f1184264e
Christ-OFF <4630125+Christ-OFF@users.noreply.github.com>
2019-06-06 23:55:25 +0200

    CHANGE - Getcontent is now async using request-promise

---
+++ _tests_/inputs/get-content-to-scrap.test.js
@@ -0,0 +1,35 @@
+/**
+ * Http mocking done thanks to :
+ * https://scotch.io/tutorials/nodejs-tests-mocking-http-requests
+ */
+
+const handler = require('../../src/inputs/get-content-to-scrap');
+const nock = require('nock');
+
+const EXPECTED_RESPONSE = "this is the expected response";
+
+describe('Testing GetRikishi HTTP ', () => {
+
+  test('Get should return the expected content', async () => {
+    expect.assertions(2);
+    // GIVEN
+    nock("http://perdu.com").get('/').reply(200, EXPECTED_RESPONSE);
+    // WHEN
+    let data = await handler.getContentToScrap("http://perdu.com", "/");
+    // THEN
+    expect(data).toBeDefined();
+    expect(data).toBe(EXPECTED_RESPONSE);
+  });
+
+  test('should return null on not found request', async () => {
+    expect.assertions(1);
+    // GIVEN
+    nock("http://perdu.com").get('/').reply(404, null);
+    // WHEN
+    let data = await handler.getContentToScrap("http://perdu.com","/");
+    // THEN
+    expect(data).toBeNull();
+  });
+
+});
+
--- src/inputs/get-content-to-scrap.js
+++ src/inputs/get-content-to-scrap.js
@@ -1,59 +1,26 @@
 'use strict';
 
-let http = require('http');
+// http module doesn't provide async/promise
+const rp = require('request-promise');
 
 /**
  * Get an URL content and return it in callback call
  * @param host not endeing with /
  * @param path starting with /
- * @param callback : data maybe null
+ * @return html content or null if an error occurred
  */
-exports.getContentToScrap = function (host, path, callback) {
+exports.getContentToScrap = async (host, path) => {
 
   const url = host + (path ? path : "");
   console.log(`Going to get : ${url}`);
-
   try {
-
-    http.get(url, (res) => {
-
-      const {statusCode} = res;
-
-      let error;
-      if (statusCode !== 200) {
-        error = new Error(`Request Failed on .${host}${host} Status Code: ${statusCode}`);
-      }
-      if (error) {
-        console.error(error.message);
-        // Consume response data to free up memory
-        res.resume();
-        callback(null);
-      } else {
-        console.log("Status is ok. Going to receive data");
-      }
-
-      res.setEncoding('utf8');
-      let rawData = '';
-      res.on('data', (chunk) => {
-        rawData += chunk;
-      });
-      res.on('end', () => {
-        try {
-          console.log(`All data retrieved ${rawData.length} bytes`);
-          callback(rawData);
-        } catch (e) {
-          console.error(e.message);
-        }
-      });
-
-    }).on('error', (e) => {
-      console.error(`Unexpected error${e.message}`);
-      console.error(e);
-    });
-
+    let html = await rp(url);
+    console.log(`All data retrieved ${html.length} bytes`);
+    return html;
   } catch (e) {
     console.error(`Unexpected http error${e.message}`);
     console.error(e);
+    return null;
   }
 
 };
